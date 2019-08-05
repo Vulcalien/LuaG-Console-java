45301b69e34779f4a121f07436b9733ce05b9285
Vulcalien <rusponirocco@gmail.com>
2019-08-05 11:58:45 +0200

    SpriteEditor's tab + EditorPanel footer

---
+++ src/vulc/luag/editor/sprite/SpriteEditor.java
@@ -0,0 +1,16 @@
+package vulc.luag.editor.sprite;
+
+import vulc.luag.Console;
+import vulc.luag.editor.Editor;
+import vulc.luag.gfx.panel.EditorPanel;
+
+public class SpriteEditor extends Editor {
+
+	public SpriteEditor(Console console, EditorPanel panel, int x, int y, int w, int h) {
+		super(console, panel, x, y, w, h);
+	}
+
+	public void tick() {
+	}
+
+}
