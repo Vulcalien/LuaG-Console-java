2dbbfcbadac1f78426fde9666a2dc0a3ddc414ca
Vulcalien <rusponirocco@gmail.com>
2019-10-06 09:22:37 +0200

    Bug Fix: 'run' command trying to run last inserted cartridge even with no argument

--- src/vulc/luag/cmd/command/RunCommand.java
+++ src/vulc/luag/cmd/command/RunCommand.java
@@ -14,12 +14,17 @@ public class RunCommand extends CmdCommand {
 
 	public void run(Cmd cmd, String[] args) {
 		Console console = cmd.console;
+
 		if(args.length >= 1) {
 			console.cartridge = args[0] + "." + Game.CARTRIDGE_EXTENSION;
-		} else if(console.mode == Mode.USER_CMD) {
-			cmd.write("Error:\n"
-			          + "insert cartridge's name\n\n");
-			return;
+		} else {
+			console.cartridge = null;
+
+			if(console.mode == Mode.USER_CMD) {
+				cmd.write("Error:\n"
+				          + "insert cartridge's name\n\n");
+				return;
+			}
 		}
 
 		console.switchToPanel(new GamePanel(console));
