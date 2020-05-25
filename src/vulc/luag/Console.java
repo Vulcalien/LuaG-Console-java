f5b624cfd54df061f88d261d8f77de0cef5f3e1b
Vulcalien <rusponirocco@gmail.com>
2020-05-25 13:57:28 +0200

    Frame: setExtendedState can be used, but using pack() removes the effect

--- src/vulc/luag/Console.java
+++ src/vulc/luag/Console.java
@@ -29,6 +29,8 @@ import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.swing.JFrame;
+
 import vulc.luag.gfx.ConsoleFrame;
 import vulc.luag.gfx.Screen;
 import vulc.luag.gfx.panel.BootPanel;
@@ -301,7 +303,7 @@ public class Console extends Canvas implements Runnable {
 
 		if(fullScreen) {
 			frame.setUndecorated(true);
-			frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+			frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		} else {
 			// minimum size: Console's size not scaled
 			frame.pack(); // this sets the insets of the frame
