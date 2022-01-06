a3ce992c1dee6c9a0a21787a5e9b70c232505b09
Vulcalien <vulcalien@gmail.com>
2022-01-06 23:09:34 +0100

    Update copyright notice in Console.java header

--- src/vulc/luag/Console.java
+++ src/vulc/luag/Console.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2019-2020 Vulcalien
+ * Copyright 2019-2022 Vulcalien
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
